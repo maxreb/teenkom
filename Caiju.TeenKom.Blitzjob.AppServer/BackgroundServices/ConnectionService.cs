99e6e71570c0c1747040a2c24cb2aa06ab552c77
Max Reble <max@reble.eu>
2020-03-21 15:58:59 +0100

    first implementation

---
+++ Caiju.TeenKom.Blitzjob.AppServer/BackgroundServices/ConnectionService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Caiju.TeenKom.Blitzjob.AppServer.BackgroundServices
+{
+	public class ConnectionService : BackgroundService
+	{
+		protected override Task ExecuteAsync(CancellationToken stoppingToken)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Caiju.TeenKom.Blitzjob.AppServer/Services/PeopleService.cs
@@ -0,0 +1,39 @@
+﻿using Caiju.TeenKom.Blitzjob.AppServer.Protos;
+using Google.Protobuf.Collections;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Caiju.TeenKom.Blitzjob.AppServer.Services
+{
+	public class PeopleService : People.PeopleBase
+	{
+
+
+		public override Task<GetCoachRes> GetCoach(GetCoachReq request, ServerCallContext context)
+		{
+
+			var ppl = new PeopleBase
+			{
+				FirstName = "Test",
+				LastName = "LastName",
+				Id = request.Id
+			};
+			ppl.Phones.Add(new PeopleBase.Types.PhoneNumber { Number = "+49 1234 56789", Type = PeopleBase.Types.PhoneType.Mobile });
+
+			return Task.FromResult(new GetCoachRes
+			{
+				Coach = new Coach
+				{
+					PeopleBase = ppl,
+					CoachTimeReachableFrom = Timestamp.FromDateTime(DateTime.Now),
+					CoachTimeReachableTo = Timestamp.FromDateTime(DateTime.Now.AddHours(3))
+
+				}
+			});
+		}
+	}
+}
