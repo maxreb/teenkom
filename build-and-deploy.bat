4d07b9f7c35f5a0c61ddf8a2c0c226cdd5f3ec24
Max Reble <max@reble.eu>
2020-03-22 20:08:46 +0100

    added docker deploy

---
+++ build-and-deploy.bat
@@ -0,0 +1,12 @@
+@echo off
+setlocal
+
+
+
+docker build -t caiju/tk3 -t hse-server01:5000/caiju/tk3 -f Caiju.TeenKom.TK3/Dockerfile .
+
+docker build -t caiju/appserver -t hse-server01:5000/caiju/appserver -f Caiju.TeenKom.Blitzjob.AppServer/Dockerfile .
+
+docker push hse-server01:5000/caiju/appserver
+docker push hse-server01:5000/caiju/tk3
+pause
